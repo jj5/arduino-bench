4f86f99d115a03713bc7cd5661efa863df1ccb23
John Elliot V <jj5@jj5.net>
2026-04-20 14:56:36 +1000

    Work, work...

--- module-monday/2026-04-20-HW-390-Capacitive-Soil-Moisture/2026-04-20-HW-390-Capacitive-Soil-Moisture.ino
+++ module-monday/2026-04-20-HW-390-Capacitive-Soil-Moisture/2026-04-20-HW-390-Capacitive-Soil-Moisture.ino
@@ -1,12 +1,3 @@
-// 2025-11-24 jj5 - this is LiquidCrystal I2C v1.1.2 by Frank de Brabander
-// 2025-11-24 jj5 - SEE: https://github.com/johnrickman/LiquidCrystal_I2C
-#include <LiquidCrystal_I2C.h>
-
-// 2025-12-01 jj5 - do not define this constant for production builds...
-//#define DEBUG
-
-// 2025-11-24 jj5 - initialize the LCD screen with I2C address 0x27, 20 columns, and 4 rows
-LiquidCrystal_I2C lcd( 0x27, 20, 4 );
 
 const int LED_PIN_R = 5; // Red
 const int LED_PIN_Y = 4; // Yellow
